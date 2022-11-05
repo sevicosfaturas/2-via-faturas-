8de6c7287c01a1c98aac2300832bb9044ef91b17
Paiva Lima <maslham.298@gmail.com>
2022-11-05 16:28:57 -0300

    indexs

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div class="wrapper wrapper--header">
+      <img src="/src/assets/fundo.png" alt="" />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,5 @@
+function Modal() {
+  return <div></div>;
+}
+
+export function Modal()
